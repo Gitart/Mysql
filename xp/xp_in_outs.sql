bf31f10155a2696a6dd7d1e23000adb6df07ca31
Arthur Savage <alerc@meta.ua>
2022-04-12 21:43:38 +0300

    Create xp_in_outs.sql

---
+++ xp/xp_in_outs.sql
@@ -0,0 +1,16 @@
+
+CREATE PROCEDURE GetCustomerLevel(
+    IN  pCustomerNumber INT, 
+    OUT pCustomerLevel  VARCHAR(20))
+BEGIN
+    DECLARE credit DECIMAL(10,2) DEFAULT 0;
+
+    SELECT creditLimit 
+    INTO credit
+    FROM customers
+    WHERE customerNumber = pCustomerNumber;
+
+    IF credit > 50000 THEN
+        SET pCustomerLevel = 'PLATINUM';
+    END IF;
+END$$
